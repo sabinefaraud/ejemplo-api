043bee6609edf8b1546303f8c695b272a6cfd91f
sabinefaraud <sabinefaraud@hotmail.com>
2017-01-10 17:56:14 +0100

    segundo paso

--- app/server.js
+++ app/server.js
@@ -9,10 +9,20 @@ var port = process.env.PORT || 8080  // establecemos nuestro puerto
 
 
 app.get('/',function(req,res) {
-    res.json({ mensaje: '¡Hola Mundo!' })  
+    res.json({ mensaje: '¡Hola Sabz!' })  
 });
 
+app.get('/cervezas', function(req, res) {
+  res.json({ mensaje: '¡A beber cerveza!' })  
+})
 
+app.post('/', function(req, res) {
+  res.json({ mensaje: 'Método post' })   
+})
+
+app.delete('/', function(req, res) {
+  res.json({ mensaje: 'Método delete' })  
+})
 
 // iniciamos nuestro servidor
 app.listen(port)
